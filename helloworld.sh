d97c00d29a47e1812a84cc8b2796781f105d0efe
Ko Ko Zin <im.kokozin@gmail.com>
2023-10-21 11:30:36 +0630

    Print each letter of WORLD in particular line

--- helloworld.sh
+++ helloworld.sh
@@ -3,4 +3,4 @@
 echo "Hello World!"
 echo "\r\n"
 echo "A sample script that usually use for a programming language"
-echo "H\r\nE\r\nL\r\nL\r\nO"
+echo "H\r\nE\r\nL\r\nL\r\nO\r\nW\r\nO\r\nR\r\nL\r\nD"
